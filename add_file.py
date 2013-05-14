0141faca43a8f5eaa0c73b1c6b54544295eb205e
Kristian <kristian.mort@aalto.fi>
2013-05-14 12:32:26 +0300

    Refactored parts of the wos.py autodoc stuff and appended some general info about add_file and send_file_to modules.

--- add_file.py
+++ add_file.py
@@ -2,21 +2,42 @@
 Created on 5.6.2012
 
 @author: neriksso
+
+@requires: ZeroMQ \n
+
+@summary: Used to add a file in the current project. 
 '''
 import sys
 import zmq
 
 def main():
-    if len(sys.argv) == 3:
-        id = sys.argv[1]
-        filepath = sys.argv[2] 
-        context = zmq.Context()     
-        socket = context.socket(zmq.REQ)
-        socket.connect ("tcp://127.0.0.1:5555")
-        command =  'add_to_project;'+str(id)+';'+str(filepath)
-        #print "sending command",command
-        socket.send (command)
-        socket.close()
+    """Main function of the sub program.
+    
+    Sub program is meant to be bound to windows explorer context menu.
+    Context menu allows the user to quickly add files to project without interacting with DiWaCS directly.
+    
+    Transmits the add_file command to DiWaCS via interprocess socket.
+    
+    :argument filepath: Path of the file to be added.
+    :type filepath: String
+    :returns: windows success code (0 on success).
+    :rtype: Integer
+    """
+    if len(sys.argv) == 2:
+        try :
+            filepath = sys.argv[1] 
+            context = zmq.Context()     
+            socket = context.socket(zmq.REQ)
+            #: Uses interprocess ZeroMQ socket to inform of the operation.
+            socket.connect ("tcp://127.0.0.1:5555")
+            command =  'add_to_project;0;'+str(filepath)
+            socket.send (command)
+            socket.close()
+            return 0
+        except :
+            pass
+    return 1
+          
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
