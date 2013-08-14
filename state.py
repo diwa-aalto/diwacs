93e13d1dd39e1d2ac148ae769a267ebcf42dc799
Nick Eriksson <nick.eriksson@aalto.fi>
2013-08-14 16:53:07 +0300

    Audiorecorder start fixed.

--- state.py
+++ state.py
@@ -131,6 +131,7 @@ class State(object):
         try:
             self.audio_recorder = threads.AudioRecorder(self)
             self.audio_recorder.daemon = True
+            self.start_audio_recorder()
         except Exception as excp:
             LOGGER.exception('Audio recorder exception: %s', str(excp))
         self.exited = False
