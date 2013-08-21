20f567e803d540ddb222b6e37218c202290ad97d
Kristian Mört <kristian.mort@aalto.fi>
2013-08-21 16:07:07 +0300

    Dirname fix.

--- diwacs.py
+++ diwacs.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     # Load up the loggers for every module.
     global LOGGER
     try:
-        os.makedirs(diwavars.CONFIG_PATH)
+        os.makedirs(os.path.dirname(diwavars.CONFIG_PATH))
     except:
         pass
     logging.config.fileConfig('logging.conf')
